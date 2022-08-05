8f03b78eacd305d0c3d4ae9476e3571a08d55ee5
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-08-05 14:36:04 +0500

    Сделал компонент vue-select

---
+++ src/components/VueSelect.vue
@@ -0,0 +1,34 @@
+<template>
+    <select 
+        class="form-select"
+        aria-label="Default select example"
+        @change="(event) => this.$emit('update:selectValue', event.target.value)"
+        v-model="selectValue"
+    >
+        <option selected value="">{{  nameSelect  }}</option>
+        <option 
+            v-for="option of options"
+            :key="option.value"
+            :value="option.value"
+        >
+            {{ option.name }}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    props: {
+       options: {
+            type: Object,
+            required: true
+       },
+       selectValue: String,
+       nameSelect: String
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/FoodWatcher.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="Food Watcher">
+    <vue-select 
+      :nameSelect="nameSelect"
+      :options="selectOptions"
+      v-model:selectValue="selectValue"
+    ></vue-select>
+    {{selectValue}}
+  </div>
+</template>
+
+<script>
+import VueSelect from "@/components/VueSelect"
+export default {
+  components: {
+    VueSelect
+  },
+  data() {
+    return {
+      selectOptions: [
+        {name: 'Сотрудники', value:"employees"},
+        {name: 'Поставщики', value:"provider"},
+        {name: 'Раздел-меню', value:"menu-section"},
+        {name: 'Блюдо', value:"dish"},
+        {name: 'Заказ', value:"order"}
+      ],
+      nameSelect: 'Выберите таблицу',
+      selectValue: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
