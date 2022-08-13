157a59d7960e6f56d8d195288b0f19625bc2b43f
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-08-13 10:30:45 +0500

    доделал модальное окно

--- src/components/ModalWindow.vue
+++ src/components/ModalWindow.vue
@@ -8,28 +8,28 @@
     <div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="ModalLabel">{{  title  }}</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
-            </div>
-            <div class="modal-body">
-                <div class="d-flex justify-content-end" v-for="food of contents" :key="food">
-                    {{  food['text']  }} 
-                    <button 
-                        class="btn btn-danger"
-                        @click="this.$emit('subtractCount', food['id']);"
-                    >
-                    - 
-                    </button><br>
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ModalLabel">{{  title  }}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
                 </div>
-                <div class="d-flex justify-content-end text-danger fs-5">
-                    {{conclusionText}}
+                <div class="modal-body">
+                    <div class="d-flex justify-content-end align-items-center" v-for="food of contents" :key="food">
+                        <p class="text-start flex-grow-1 p-0 m-0">{{  food['text']  }}</p> 
+                        <button 
+                            class="btn btn-danger pt-0 pb-0 ps-2 pe-2 ms-2"
+                            @click="this.$emit('subtractCount', food['id']);"
+                        >
+                        - 
+                        </button><br>
+                    </div>
+                    <div class="d-flex justify-content-end text-danger fs-5">
+                        {{conclusionText}}
+                    </div>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
-                <button type="button" class="btn btn-primary">{{  buttonText  }}</button>
-            </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                    <button type="button" class="btn btn-primary">{{  buttonText  }}</button>
+                </div>
             </div>
         </div>
     </div>
